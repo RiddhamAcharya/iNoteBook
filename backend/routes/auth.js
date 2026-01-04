e69869eb070274c79d2d9546601ff2cdf5ce3260
Riddham Acharya <myselfriddham@gmail.com>
2026-01-04 20:22:29 +0545

    Checking for unique users

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
 
-// Create a User using: POST "/api/auth/". No login required
-router.post('/', (req, res) => {
-  console.log(req.body);
-  const user = User(req.body);
-  user.save();
-  res.send(req.body);
+// Create a User using: POST "/api/auth/createuser". No login required
+router.post('/createuser',[
+  // If there are errors, return Bad request and the errors
+  body('name', 'Enter a valid name').isLength({ min: 3 }),
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Password must be at least 5 characters').isLength({ min: 5 }),
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+// Check if the user exists already
+try{
+  let user = await User.findOne({email: req.body.email});
+  if(user){
+    return res.status(400).json({error: "Sorry a user with this email already exists"})
+  }
+  user = await User.create({ 
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  })
+
+  res.json(user);
+}catch(error){
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = router;
