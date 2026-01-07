e72845569ddee058528117aeac4d45b6fe8d8c12
Riddham Acharya <myselfriddham@gmail.com>
2026-01-07 17:21:28 +0545

    Password hashing

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+const JWT_SECRET = 'Harryisagoodb$oy';
 
 // Create a User using: POST "/api/auth/createuser". No login required
 router.post('/createuser',[
@@ -21,13 +24,26 @@ try{
   if(user){
     return res.status(400).json({error: "Sorry a user with this email already exists"})
   }
+  const salt = await bcrypt.genSalt(10);
+  const secPass = await bcrypt.hash(req.body.password, salt);
+  // Create a new user
   user = await User.create({ 
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password
-  })
+    password: secPass
+  });
 
-  res.json(user);
+  const data = {
+    user: {
+      id: user.id
+    }
+  };
+
+  const authToken = jwt.sign(data, JWT_SECRET);
+  console.log(authToken);
+  res.json({authToken});
+  // res.json(user);
+
 }catch(error){
     console.error(error.message);
     res.status(500).send("Internal Server Error");
