30a564fa395a1b60e060b39229d306f78c0933c8
Riddham Acharya <myselfriddham@gmail.com>
2026-01-15 15:11:47 +0545

    Using LocationHook

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,9 @@
 import React, {useContext, useEffect} from 'react'
-import NoteContext from '../context/notes/NoteContext'
 
-const About = () => {
-  const a = useContext(NoteContext);
-  useEffect(()=>{
-    a.update();
-    // eslint-disable-next-line
-  }, []);
+const About = () => {  
   return (
     <div>
-    This is about {a.state.name} and he is in class {a.state.class}
+    This is about page
     </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,21 +2,8 @@ import { useState } from 'react';
 import NoteContext from './NoteContext';
 
 const NoteState = (props)=>{
-    const s1 ={
-        "name":"Harry",
-        "class": "5b"
-    };
-    const [state, setState] = useState(s1);
-    const update = ()=>{
-        setTimeout(() => {
-            setState({
-                "name":"Rohan",
-                "class":"10b"
-            })
-        }, 2000);
-    }
     return(
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )
