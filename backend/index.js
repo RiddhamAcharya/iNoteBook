a246ba09db3496e3946286bf860d06302cdfb484
Riddham Acharya <myselfriddham@gmail.com>
2026-01-21 17:15:49 +0545

    SignUp Components

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ app.use(express.json());
 // Available Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
+
 app.listen(port, () => {
   console.log(`iNoteBook backend listening on http://localhost:${port}`);
 });
